11134c631f4f8dcfd4b5b38f2a5b64a43af5bc52
debasish-max <hazarikadebasish182@gmail.com>
2025-09-16 20:14:59 +0530

    update

---
+++ Javascripts/swiper.js
@@ -0,0 +1,13 @@
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 0,
+      centeredSlides: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+    });
