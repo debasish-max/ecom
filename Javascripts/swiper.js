4f32ac43fe973ccfade27fb2cf899cc352214127
debasish-max <hazarikadebasish182@gmail.com>
2025-09-18 23:03:15 +0530

    Save local changes before rebase

--- Javascripts/swiper.js
+++ Javascripts/swiper.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
     var swiper = new Swiper(".mySwiper", {
+=======
+    var swiper = new Swiper(".swiper", {
+>>>>>>> e22906b (updated)
       spaceBetween: 0,
       centeredSlides: true,
       pagination: {
