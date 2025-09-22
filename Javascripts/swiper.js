6060320bfc6d73869a522d9f82f2484a1fc78880
debasish-max <hazarikadebasish182@gmail.com>
2025-09-22 11:42:52 +0530

    added

--- Javascripts/swiper.js
+++ Javascripts/swiper.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-    var swiper = new Swiper(".mySwiper", {
-=======
     var swiper = new Swiper(".swiper", {
->>>>>>> e22906b (updated)
       spaceBetween: 0,
       centeredSlides: true,
       pagination: {
